478e3dbb6f74d224c234024feddff72c8866913b
Claudio Cabral <cl@udio.co>
2019-03-26 18:01:23 +0100

    better mymounts

--- j09/mymounts.c
+++ j09/mymounts.c
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: GPL-2.0+
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/dcache.h>
 #include <linux/uaccess.h>
 #include <linux/mount.h>
 #include <linux/fs_struct.h>
 #include <linux/list.h>
 #include <linux/seq_file.h>
+#include <linux/limits.h>
+#include <../fs/mount.h>
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Claudio Cabral <cl@udio.co>");
@@ -14,50 +18,49 @@ MODULE_DESCRIPTION("This module talks to Morty");
 
 static struct proc_dir_entry *g_file;
 static char g_buffer[PAGE_SIZE];
-static char g_current_dir[PAGE_SIZE];
-static loff_t g_buffer_size;
+static char path[PATH_MAX];
 
-static void append_to_buffer(const char *entry, const char *parent, loff_t *pos)
+static const struct seq_operations seqops = {
+	.start = NULL,
+	.next = NULL,
+	.stop = NULL,
+	.show = NULL
+};
+
+static void append_to_buffer(struct mount *entry)
 {
-	strlcat(g_buffer, entry, PAGE_SIZE);
-	strlcat(g_buffer, "\t", PAGE_SIZE);
-	if (*parent) {
-		strlcat(g_buffer, parent, PAGE_SIZE);
-		strlcat(g_buffer, "/", PAGE_SIZE);
-	}
-	strlcat(g_buffer, entry, PAGE_SIZE);
+	char *real_path;
+
+	real_path = dentry_path_raw(entry->mnt_mountpoint, path, PATH_MAX);
+	if (IS_ROOT(entry->mnt_mountpoint))
+		strlcat(g_buffer, "root", PAGE_SIZE);
+	else
+		strlcat(g_buffer, entry->mnt_devname, PAGE_SIZE);
+	strlcat(g_buffer, " ", PAGE_SIZE);
+	strlcat(g_buffer, real_path, PAGE_SIZE);
 	strlcat(g_buffer, "\n", PAGE_SIZE);
 }
 
-static void recursive_append(struct dentry *entry, char *parent)
+static void recursive_append(struct mount *mnt)
 {
-	struct dentry *current_entry;
-	size_t len;
+	struct mount *current_entry;
 
-	if (!entry)
-		return ;
-	append_to_buffer(entry->d_name.name, parent, &g_buffer_size);
-	strlcat(parent, entry->d_name.name, PAGE_SIZE);
-	len = strlen(parent);
+	append_to_buffer(mnt);
 	list_for_each_entry(current_entry,
-			&current->fs->root.mnt->mnt_root->d_subdirs, d_child) {
-		if (current_entry->d_flags & DCACHE_MOUNTED) {
-			parent[len] = 0;
-			//recursive_append(current_entry, parent);
-		}
+			&mnt->mnt_mounts, mnt_child) {
+			append_to_buffer(current_entry);
 	}
 }
 
 static void update_mounts(void)
 {
 	*g_buffer = 0;
-	*g_current_dir = 0;
-	recursive_append(current->fs->root.mnt->mnt_root, g_current_dir);
-
+	recursive_append(real_mount(current->fs->root.mnt));
 }
-static ssize_t mymounts_open(struct file *filp, char __user *buf, size_t count)
+
+static ssize_t mymounts_open(struct inode *node, struct file *filp)
 {
-	return 0;
+	return seq_open(filp, &seqops);
 }
 
 static ssize_t mymounts_read(struct file *filp, char __user *buf, size_t count,
@@ -77,15 +80,16 @@ static ssize_t mymounts_write(struct file *filp, const char __user *buf, size_t 
 
 static const struct file_operations fops = {
 	.owner = THIS_MODULE,
-	.open = mymounts_open,
-	.read = seq_read,
-	.write = seq_write
+	// .open = mymounts_open,
+	.read = mymounts_read,
+	.write = mymounts_write
 };
+
 
 static __init int init_debug(void)
 {
 
-	g_file = proc_create("mymount", 0444, NULL, &fops);
+	g_file = proc_create("mymounts", 0444, NULL, &fops);
 	if (!g_file)
 		return -ENODEV;
 	return 0;
